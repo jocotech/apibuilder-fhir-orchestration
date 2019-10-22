084e3561f193f271871e75214ce3fcf44605cf38
SSC <ssc@axway.com>
2019-10-22 09:15:25 -0700

    initial

---
+++ conf/Patient.default.js
@@ -0,0 +1,17 @@
+module.exports = {
+	// The configuration settings for your Swagger service.
+	pluginConfig: {
+		'@axway/api-builder-plugin-fn-swagger': {
+			'Patient': {
+				// It is possible to override Swagger URI options when constructing
+				// outbound requests from the Swagger plugin.
+				uri: {
+					// protocol: 'https',
+					// host: 'hostname',
+					// port: 443,
+					// basePath: '/api'
+				}
+			}
+		}
+	}
+};
